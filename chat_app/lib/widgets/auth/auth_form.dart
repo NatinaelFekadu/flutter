e737b40ed68fb719338edceb66baab74ceeb620e
unknown <natnaelfekadu801@gmail.com>
2023-09-12 15:41:36 -0700

    firebase beyond the dammy backend!

---
+++ chat_app/lib/widgets/auth/auth_form.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  final void Function(
+    String email,
+    String username,
+    String password,
+    bool isLogin,
+    BuildContext context,
+  ) _submitFn;
+  final bool isLoading;
+  const AuthForm(this._submitFn, this.isLoading, {super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  String _userEmail = '';
+  String _userName = '';
+  String _userpassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+    if (isValid) {
+      _formKey.currentState!.save();
+      widget._submitFn(
+        _userEmail.trim(),
+        _userName.trim(),
+        _userpassword.trim(),
+        _isLogin,
+        context,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      key: const ValueKey('email'),
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: 'Email address',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty || !value.contains('@')) {
+                          return 'please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _userEmail = newValue!;
+                      },
+                    ),
+                    if (!_isLogin)
+                      TextFormField(
+                        key: const ValueKey('username'),
+                        decoration: const InputDecoration(
+                          labelText: 'Username',
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 4) {
+                            return 'please enter at least four characters';
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          _userName = newValue!;
+                        },
+                      ),
+                    TextFormField(
+                      key: const ValueKey('password'),
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 7) {
+                          return 'password must be at least 7 character long';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _userpassword = newValue!;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    if (widget.isLoading) const CircularProgressIndicator(),
+                    if (!widget.isLoading)
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: Size(
+                                Theme.of(context).buttonTheme.minWidth,
+                                Theme.of(context).buttonTheme.height),
+                            shape: Theme.of(context).buttonTheme.shape
+                                as OutlinedBorder),
+                        onPressed: _trySubmit,
+                        child: Text(_isLogin ? 'Login' : 'Signup'),
+                      ),
+                    if (!widget.isLoading)
+                      TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(_isLogin
+                              ? 'Create new account'
+                              : 'I already have an account'))
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ chat_app/lib/widgets/chat/message_bubble.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String message;
+  final bool isMe;
+  final Key? uniqueKey;
+  const MessageBubble(this.message, this.isMe, {this.uniqueKey, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: isMe
+                  ? Colors.grey[300]
+                  : Theme.of(context).floatingActionButtonTheme.backgroundColor,
+              borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(12),
+                  topRight: const Radius.circular(12),
+                  bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+                  bottomRight:
+                      isMe ? Radius.circular(0) : Radius.circular(12))),
+          width: 140,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          child: Text(
+            message,
+            style: TextStyle(color: isMe ? Colors.black : Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+}
